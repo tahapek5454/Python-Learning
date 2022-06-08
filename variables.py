32238d32f36f3f0685a252017481a72cf341cf2c
Taha Pek <tahapek5454@gmail.com>
2022-06-08 12:38:25 +0300

    add variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+
+
+# Degisken tanımlama kuralları
+# Rakamla baslamayamaz
+number = 1
+print(number)
+
+print(number+30)
+
+# Buyuk Kucuk duyarlılıgı vardır
+
+age = 15
+AGE = 20
+print(age)
+print(AGE)
+
+x = 5 # integer
+y = 5.0 # float
+flag = True # boolean
+name = 'taha' # String
+
+x , y, flag , name = (5,5.0,True,'taha')  # toplu tanımlama sekli
