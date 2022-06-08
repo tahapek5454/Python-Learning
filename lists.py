2c0778d171c8c7cbbf62913bd114d67206ea09e2
Taha Pek <tahapek5454@gmail.com>
2022-06-08 20:59:45 +0300

    add lists

---
+++ lists.py
@@ -0,0 +1,34 @@
+
+my_list = [1,2,3]
+
+print(my_list)
+
+# listedeki elemanların veri tipi birbirinden farklı olabilir
+
+my_list = [1,'ali',False]
+
+print(my_list)
+
+# listeler stringler gibi toplanabilir
+
+number1 = ['one','two','three']
+number2 = ['four','five','six']
+
+numbers = number1+number2
+
+print(numbers)
+print(len(numbers))
+
+# liste icersinde liste seklinde toplamak icin
+
+user1 =['taha',19]
+user2 =['ahmet',17]
+
+users = [user1,user2]
+
+print(users)
+print(users[1])
+
+# user1 icersindeki 1 . indexe ulasmak icin
+
+print(users[1][1])
