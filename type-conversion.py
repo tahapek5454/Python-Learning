40b765454efedab1926a45dfa6a4cfe2c7a9ae2f
Taha Pek <tahapek5454@gmail.com>
2022-06-08 12:54:50 +0300

    add type-conversion

---
+++ type-conversion.py
@@ -0,0 +1,57 @@
+
+
+# kalvyeden deger okuma input metoduyla gerceklesir
+
+# x = input("sayi giriniz:")
+# y = input("sayi giriniz: ")
+
+# print(x+y)
+
+# print(type(x))
+# print(type(y))
+
+# #input metodu bizden string degerler alir type conversion islemi uygalaycagiz
+
+# toplam = int(x) + int(y)
+# print(type(x))
+# print(type(y))
+
+# print(toplam)
+
+x = 5 # int
+y = 6.0 #float
+flag = True # boolean
+name = 'Taha' #string
+
+# type - conversion
+
+# int to flaot
+
+x = float(x)
+print(type(x))
+print(x)
+
+# float to int
+
+y = int(y)
+print(type(y))
+print(y)
+
+# bool to String
+
+flag = str(flag)
+print(type(flag))
+print(flag)
+
+# ornek
+
+# yari capi verilen bir dairenin alanini hesaplayiniz (pi = 3.14)
+
+r = float(input("yari capi giriniz : "))
+
+print("alan = ")
+print(r**2*3.14)   # ** ust anlamina gelir
+
+
+
+ 
