e4ac1b3601122fa1eca4991f2e85c75fb584141f
Taha Pek <tahapek5454@gmail.com>
2022-06-12 10:26:49 +0300

    add methods

---
+++ methods.py
@@ -0,0 +1,14 @@
+
+# methods
+
+# bir class icersinde yapılan script
+
+liste = [1,2,3]
+
+liste.append(5) # append bir metod
+
+print(liste)
+
+# function
+
+# class icersinde olmayan bir scrip karıştırılmamalı
