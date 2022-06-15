f0d21a467143df4c3b6c2698f03c9e53cd7e9b81
Taha Pek <tahapek5454@gmail.com>
2022-06-15 15:24:17 +0300

    add my-module folder

---
+++ my-module/mod.py
@@ -0,0 +1,26 @@
+
+number = 19
+
+print("Modul Basariyla tanımlandi")
+
+'''
+Bu modul benim tanimladigim bir moduldur
+'''
+
+my_list = [1,2,3,4,5]
+
+my_dict = {
+    "name":"taha" ,
+    "surname":"pek",
+    "age":20
+}
+
+def sayHello():
+    print("hello")
+
+
+
+class Temp:
+    
+    def sinif_fonk(self):
+        print("Sinifin fonksiyonuna ulastin")
---
+++ my-module/my-module-train.py
@@ -0,0 +1,16 @@
+import mod
+
+# value = help(mod)
+
+print(mod.number)
+
+print(mod.my_list)
+
+print(mod.my_dict["name"])
+
+mod.sayHello()
+
+nesne = mod.Temp()
+
+nesne.sinif_fonk()
+
