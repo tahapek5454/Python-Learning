8404472a4912cbceface62dc84303c8420467d5a
Taha Pek <tahapek5454@gmail.com>
2022-07-09 19:35:22 +0300

    remove driver

--- instagram-bot/_instagramLogin.py
+++ instagram-bot/_instagramLogin.py
@@ -12,7 +12,7 @@ password = "YourPassword"
 
 class Instagram:
     def __init__(self,username,password) -> None:
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(r'C:\Users\90543\Desktop\VS\python\web-driver\chromedriver.exe')
         self.userName = username
         self.password = password
     
