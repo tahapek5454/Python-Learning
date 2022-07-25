5df4e0d86c2baa83e028c47f82691bedfaaf71ec
Taha Pek <tahapek5454@gmail.com>
2022-07-25 22:31:42 +0300

    add select

---
+++ sql-database/_select.py
@@ -0,0 +1,14 @@
+
+import mysql.connector
+
+conn = mysql.connector.connect(host='localhost', user = 'root', password = '', database = 'node_app')
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT name , price FROM products')
+
+result = cursor.fetchall()   # tek bir veri gelmesi icin fetchone
+
+for i in result:
+    print(f'Name = {i[0]} Price = {i[1]}')
+
