174c6345a99f6bfa01fc19f96902f08202906f14
Taha Pek <tahapek5454@gmail.com>
2022-07-30 13:36:56 +0300

    update class

--- sql-database/school-project/_Class.py
+++ sql-database/school-project/_Class.py
@@ -6,4 +6,21 @@ class Class:
         self.id = id
         self.teacher_id = teacher_id
         self.name = name
+    
+
+    @staticmethod
+    def createClass(obj):
+
+        liste = []
+
+        if isinstance(obj,tuple):
+
+            return Class(obj[0],obj[1],obj[2])
+        else :
+
+            for objj in obj:
+
+                liste.append(Class(objj[0],objj[1],objj[2]))
+            
+            return liste
         
