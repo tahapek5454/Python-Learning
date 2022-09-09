e5d86197c15963e7e030f27a54a03246f5f6d379
Taha Pek <tahapek5454@gmail.com>
2022-09-09 17:34:54 +0300

    update django/catalog

---
+++ django/catalog/pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# http://127.0.0.1:8000/
+# benim localhostumdaki 8000 portunda yayımalnacak
+
+urlpatterns = [
+    path('', views.index, name='index') # ilk parametrenin bos olmasi ana hostta kalamamız
+]
