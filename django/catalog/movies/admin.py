1230f74ca354ae9328c8fd6bb31e1abe8208e572
Taha Pek <tahapek5454@gmail.com>
2022-09-10 17:47:29 +0300

    add movie table

--- django/catalog/movies/admin.py
+++ django/catalog/movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Movie
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'create_date', 'isPublished')
+    list_display_links = ('id','name')
+    list_filter = ('create_date',)
+    list_editable = ('isPublished',)
+    search_fields = ('name', 'description',)
+
+admin.site.register(Movie, MovieAdmin)
--- django/catalog/movies/models.py
+++ django/catalog/movies/models.py
@@ -1,3 +1,17 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Film Adi')
+    description = models.TextField(verbose_name='Film Aciklamasi')
+    image = models.CharField(max_length=50, verbose_name='Film Fotografi')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Yukleme Tarihi')
+    isPublished = models.BooleanField(default=True)
+   
+
+
+    def __str__(self) -> str:
+        return self.name
+
