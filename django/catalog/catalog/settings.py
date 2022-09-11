795c58d00094994c8afaea85266002fa103fba4d
Taha Pek <tahapek5454@gmail.com>
2022-09-11 13:06:58 +0300

    add user and update

--- django/catalog/catalog/settings.py
+++ django/catalog/catalog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
     'pages.apps.PagesConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
