816446ab1875bd52e76733e93586e984aa944b72
Taha Pek <tahapek5454@gmail.com>
2022-09-22 10:16:01 +0300

    add templates for blog

--- django/blogapp/blog/views.py
+++ django/blogapp/blog/views.py
@@ -9,13 +9,22 @@ from django.shortcuts import render
 
 # request parametresinin gelmesi bu sayfaların açılması için bir isteğe ihtiyaç duyması
 
+# render bize html dosyalarını göndermemizi sağlıyor
+# render a request ve ilgili templates klosorundeki html sayfalarını gönderiyor
+# tüm uygulamalardaki templates kolosorlerine baktığından karışmaması adına biz klosor klosor yapalım
+
 def index(request):
-    return HttpResponse("Home Page")
+    return render(request, "blog/index.html")
 
 def blogs(request):
-    return HttpResponse('Blogs Page')
+    return render(request, 'blog/blogs.html')
 
 def blogs_details(request, id):
     # block details ın id alma sebebi url kısmında değişken bir <int:id> deger gelecegini 
     # belirtmemiz gelen degeree gore islem yapacagiz
-    return HttpResponse('Blogs-details id : '+ str(id))
+
+    # bizim parametre olarak gelen id yi kullanammız icin 3. parametreye key value seklinde
+    # gelen id yi gondermemiz lazım
+    return render(request, "blog/blogs_details.html", {
+        "id": id,
+    })
