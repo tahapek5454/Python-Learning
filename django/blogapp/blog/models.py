3dacd31e81bda63c6e0a065e1149ac4fb6cf16e5
Taha Pek <tahapek5454@gmail.com>
2022-09-24 19:10:48 +0300

    add models for databse

--- django/blogapp/blog/models.py
+++ django/blogapp/blog/models.py
@@ -1,3 +1,23 @@
+from statistics import mode
+from turtle import title
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+
+# model bizim databaseimizdeki tablolara dek dusuyor
+
+# gecen yaptıgımız views icersindeki data dict ini buraya koyalım
+# ve onu veri tabanına baglayalım
+
+class Blog (models.Model):
+    # id otomatik veriliyor
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
+    description = models.TextField(null=True)
+
+
+class Category (models.Model):
+    name = models.CharField(max_length=150)
