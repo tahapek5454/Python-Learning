888fd43328a82e47b8d1a1f78017af28afa1b370
Taha Pek <tahapek5454@gmail.com>
2022-10-02 15:05:30 +0300

    add register for account

--- django/blogapp/account/views.py
+++ django/blogapp/account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 # authenticate method bize gelen username passwordun giris yapıp yapacagını bulmaya yarıyor
 # login iste bizim tarayıcı üzerinde sessiona drekt cookie yi ekliyor hesanı loginletiyor
+from django.contrib.auth.models import User
+# bu class bizim userlar uzerinden islem yapmaıza ve user eklememizi saglayacak
 
 # Create your views here.
 
@@ -34,6 +36,41 @@ def login_request(request):
 
 
 def register_request(request):
+
+    if request.method == "POST":
+        email = request.POST["email"]
+        firstName = request.POST["firstname"]
+        lastName = request.POST["lastname"]
+        userName = request.POST["username"]
+        password = request.POST["password"]
+        rePassword = request.POST["repassword"]
+    
+        if password == rePassword:
+            if User.objects.filter(username = userName).exists():
+                # bu metodu biz bu isimle kayutlu bir kullanıcı dah once var mı diye kontrol ediyoruz
+                return render(request, "account/register.html", {
+                "eror" : "Kullanici adi daha onceden olusturulmustur."
+                })
+            elif User.objects.filter(email = email).exists():
+                return render(request, "account/register.html", {
+                "eror" : "Email daha onceden olusturulmustur."
+                })
+            else:
+                # her sey yolundadir bir user olusturalım
+                user  = User.objects.create_user(username=userName, email=email, first_name = firstName,
+                                                last_name =lastName, password=password )
+                # burda User objesinin tanımına gidip parametreli ogrenebiilirs inheritance
+                # create_user sifreleri falan hashleyerek yolluyor
+                user.save() # ile de kayıt ettik
+
+                return redirect('login')
+
+
+
+        else:
+            return render(request, "account/register.html", {
+                "eror" : "Sifreler uyusmuyor. Lutfen kontrol ediniz"
+            })
 
     return render(request, "account/register.html")
 
