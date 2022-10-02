07d65c1ee684260e51760d1ce31dac8f6ec385ea
Taha Pek <tahapek5454@gmail.com>
2022-10-02 14:27:14 +0300

    add login panel for account

--- django/blogapp/account/views.py
+++ django/blogapp/account/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import redirect, render
-from requests import request
+from django.contrib.auth import authenticate, login
+# authenticate method bize gelen username passwordun giris yapıp yapacagını bulmaya yarıyor
+# login iste bizim tarayıcı üzerinde sessiona drekt cookie yi ekliyor hesanı loginletiyor
 
 # Create your views here.
 
 
 def login_request(request):
+
+    # biz eger post islemi yapıldsıysa if blogu icersindekileri
+    # gerceklestirecegiz bunu kontorlu request icersindeki methoddan bakacagız
+    # akti takdirde bu ssayfaya istek geldiyse sayfa arayuzunu gosterecegiz
+
+    if request.method == "POST":
+        userName = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = userName, password = password)
+        # eger boyle bir kullanıcı varsa giris payabilir bize o kullanucu dondurur
+
+        if user is not None:
+            login(request, user)
+            # login i istegi ve kullanıcıyı veridk
+            return redirect("home") # kayıt olunda home dan deva
+        else:
+            return render(request, "account/login.html", {
+                "eror": "Kullanici adi veya sifre yanlis"
+            })
+            # boyle bir kullanıcı yoksa tekrar loin sayfasını verelim
+            # ve bir error atalım
 
     return render(request, "account/login.html")
 
